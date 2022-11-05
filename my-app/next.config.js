cea12f6572569cc3c01eae213ca2ad505fc18f71
pfed-prog <fedorovn19@gmail.com>
2022-11-05 16:22:51 +0100

    added rewrites

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,16 +1,56 @@
 /** @type {import('next').NextConfig} */
 
-/* module.exports = {
+module.exports = {
   async rewrites() {
     return [
       {
-        source: "/blog",
+        source: "/",
+        destination: "/index.html",
+      },
+      {
+        source: "/404",
+        destination: "/404.html",
+      },
+      {
+        source: "/about",
         destination: "/about.html",
       },
       {
-        source: "/",
-        destination: "/about.html",
+        source: "/archive",
+        destination: "/archive.html",
+      },
+      {
+        source: "/author",
+        destination: "/author.html",
+      },
+      {
+        source: "/category",
+        destination: "/category.html",
+      },
+      {
+        source: "/contact",
+        destination: "/contact.html",
+      },
+      {
+        source: "/date",
+        destination: "/date.html",
+      },
+      {
+        source: "/page",
+        destination: "/page.html",
+      },
+      {
+        source: "/search",
+        destination: "/search.html",
+      },
+      {
+        source: "/single",
+        destination: "/single.html",
+      },
+      {
+        source: "/tag",
+        destination: "/tag.html",
       },
     ];
   },
-}; */
+};
